9817fec2080d1085c4d194f4155a22821e33972d
pmarshall111 <pmarshall.dev@gmail.com>
2022-02-12 19:09:40 +0000

    FB-43 Updating title and adding Google Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
-import logo from './logo.svg';
+import ReactGA from 'react-ga';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import LandingPage from "./intro/landing-page/LandingPage";
-import FloatingBetslips from "./intro/landing-page/FloatingBetslips";
-import PerformancePage from "./intro/performance-page/PerformancePage";
-import PieChart from "./common/PieChart";
-import Filters from "./intro/performance-page/Filters";
-import FaqPage from "./intro/faq-page/FaqPage";
-import TitleBreak from "./common/TitleBreak";
 import NavbarComponent from "./common/NavbarComponent";
-import ManualSignUp from "./dashboard/sign-in-popup/ManualSignUp";
-import SigninPopup from "./dashboard/sign-in-popup/SigninPopup";
-import YourStats from "./dashboard/account-page/YourStats";
-import Dashboard from "./dashboard/Dashboard";
 import Intro from "./intro/Intro";
 
+const GOOGLE_ANAYTICS_TAG = "G-QNTTPNDQMK"
+
 function App() {
+    useEffect(() => {
+        document.title = "Beating The Bookies"
+    }, [])
+
+    ReactGA.initialize(GOOGLE_ANAYTICS_TAG)
     return (
         <Router>
             <div className="App">
